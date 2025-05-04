fae6f9a96d8d49477c29f99d27526719336b8eb6
cmuvr <smuvr90@gmail.com>
2025-05-04 13:33:00 +0200

    show email

--- resources/views/filament/components/topbar.blade.php
+++ resources/views/filament/components/topbar.blade.php
@@ -1,3 +1,11 @@
+
+@php
+    use App\Models\User;
+    /**
+     * @var User $user
+     */
+    $user = auth()->user();
+@endphp
 <div class="flex items-center gap-4 text-emerald-600">
-    {{auth()->user()->name}}!
+    {{$user->email}}
 </div>
