0d38276796e16f52aad6fd09983f7c56f87794a0
cmuvr <smuvr90@gmail.com>
2025-05-07 18:48:56 +0200

    fix style

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [
-                ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
-                'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
-                'resources/views/**/*.blade.php',
-            ],
+            refresh: true,
         }),
+        tailwindcss(),
     ],
-})
+});
