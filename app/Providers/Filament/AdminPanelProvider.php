066ca4f054cc90e72115823fadfd98da7e142f45
cmuvr <smuvr90@gmail.com>
2025-07-20 13:55:46 +0200

    change color

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Teal,
+                'primary' => Color::Cyan,
             ])
             ->navigationGroups([
                 'Shop',
