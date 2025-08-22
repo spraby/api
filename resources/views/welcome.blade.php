cf4640cebdc10a2befb98bd1a2d68a005c04085e
cmuvr <smuvr90@gmail.com>
2025-08-22 22:09:44 +0200

    hot fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1468,7 +1468,6 @@
 </head>
 <body class="bg-black relative">
 <p class=" absolute left-[20%] top-[60%]">
-    @livewire('image-picker')
     <span class="text-white text-[78px] block leading-10">SPRABY</span>
     <span class="text-[#ccc] text-[36px] block text-right">admin</span>
 </p>
