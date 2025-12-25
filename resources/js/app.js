4170fca1c5c69bfe3c3c1745f71ef71c5bd10b55
cmuvr <smuvr90@gmail.com>
2025-12-25 16:47:38 +0100

    init

--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-import '../css/app.css'; // Импорт стилей с Tailwind 4
-import './bootstrap';
-import { createApp } from 'vue';
-import ExampleComponent from '@/components/ExampleComponent.vue';
-
-const app = createApp({});
-app.component('example-component', ExampleComponent);
-app.mount('#app');
