c4872d0b6f504750a032d3776b16a63cc0a67488
cmuvr <smuvr90@gmail.com>
2025-12-27 18:17:14 +0100

    hot fix

--- resources/js/admin/layouts/AdminLayout.tsx
+++ resources/js/admin/layouts/AdminLayout.tsx
@@ -1,15 +1,16 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { SiteHeader } from "@/components/site-header"
+import {AppSidebar} from "@/components/app-sidebar"
+import {SiteHeader} from "@/components/site-header"
 import {
     SidebarInset,
     SidebarProvider,
 } from "@/components/ui/sidebar"
-export default function Page({children}: {children: React.ReactNode, title?: string}) {
+
+export default function Page({children}: { children: React.ReactNode, title?: string }) {
     return (
         <SidebarProvider>
-            <AppSidebar />
+            <AppSidebar variant={'inset'}/>
             <SidebarInset>
-                <SiteHeader />
+                <SiteHeader/>
                 <div className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
                     {children}
                 </div>
