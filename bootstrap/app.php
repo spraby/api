82b92da8665051c9fefa8eff07dd6abc40b6baab
ihnat-els <ihnat.k@elastic-soft.com>
2026-04-11 08:37:37 +0200

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*', headers:
+            Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+        );
+
         $middleware->alias([
             'inertia' => \App\Http\Middleware\HandleInertiaRequests::class,
             'setlocale' => \App\Http\Middleware\SetLocale::class,
